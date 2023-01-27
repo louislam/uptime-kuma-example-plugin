9a81bf75c521d76f65bd2aa2c2914d6fa037090e
Louis Lam <louislam@users.noreply.github.com>
2023-01-27 17:21:13 +0800

    Add files

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { log } = require("../../../src/util");
 class ExamplePlugin extends Plugin {
 
     server;
+    loaded = false;
 
     /**
      *
@@ -18,12 +19,19 @@ class ExamplePlugin extends Plugin {
         log.debug("example-plugin", "Load event");
 
         this.server.app.get("/example-plugin-route", (req, res) => {
-            res.send("Hi, this route is added by a plugin.");
+            if (!this.loaded) {
+                res.status(500).send("Plugin is not loaded.");
+            } else {
+                res.send("Hi, this route is added by a plugin.");
+            }
         });
+
+        this.loaded = true;
     }
 
     async unload() {
         log.debug("example-plugin", "Unload event");
+        this.loaded = false;
     }
 }
 
